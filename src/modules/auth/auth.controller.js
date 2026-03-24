89950d2515d2d1c91104d03bfa558a028c2b3a80
sangram <balsangram1@gmail.com>
2026-03-24 15:40:44 +0530

    UPD : update login data

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -17,50 +17,50 @@ const RegisterUser = asyncHandler(async (req, res) => {
 });
 
 const sendEmailOTP = asyncHandler(async (req, res) => {
-  await sendEmailOTPService(req.body);
+    await sendEmailOTPService(req.body);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      null,
-      "Email OTP sent successfully"
-    )
-  );
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            null,
+            "Email OTP sent successfully"
+        )
+    );
 });
 const verifyEmailOTP = asyncHandler(async (req, res) => {
-  await verifyEmailOTPService(req.body);
+    await verifyEmailOTPService(req.body);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      null,
-      "Email OTP verified successfully"
-    )
-  );
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            null,
+            "Email OTP verified successfully"
+        )
+    );
 });
 
 const sendPhoneOTP = asyncHandler(async (req, res) => {
-  await sendPhoneOTPService(req.body);
+    await sendPhoneOTPService(req.body);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      null,
-      "Phone OTP sent successfully"
-    )
-  );
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            null,
+            "Phone OTP sent successfully"
+        )
+    );
 });
 
 const verifyPhoneOTP = asyncHandler(async (req, res) => {
-  await verifyPhoneOTPService(req.body);
+    await verifyPhoneOTPService(req.body);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      null,
-      "Phone OTP verified successfully"
-    )
-  );
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            null,
+            "Phone OTP verified successfully"
+        )
+    );
 });
 
 const LoginUser = asyncHandler(async (req, res) => {
@@ -86,7 +86,7 @@ const VerifyOTP = asyncHandler(async (req, res) => {
             new ApiResponse(
                 200,
                 {
-                    data: result, // Don't return user data for security reasons
+                    result, // Don't return user data for security reasons
                 },
                 "OTP verified successfully"
             )
@@ -119,7 +119,7 @@ const UpdateUserProfile = asyncHandler(async (req, res) => {
                 "User profile updated successfully"
             )
         );
- });
+});
 const DeleteUser = asyncHandler(async (req, res) => {
     console.log("🚀 ~ req.body:", req.user._id)
     const result = await DeleteUserService(req.user);
@@ -170,9 +170,9 @@ const ToggleNotifications = asyncHandler(async (req, res) => {
             )
         );
 });
-const ContactSupport = asyncHandler(async (req, res) => { 
+const ContactSupport = asyncHandler(async (req, res) => {
     const result = await ContactSupportService(req, res);
-     return res
+    return res
         .status(200)
         .json(
             new ApiResponse(

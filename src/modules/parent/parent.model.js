87807dfe32176a9faaa33f589224d5a0ea9ba8be
sangram <balsangram1@gmail.com>
2026-05-28 11:48:41 +0530

    updated default

--- src/modules/parent/parent.model.js
+++ src/modules/parent/parent.model.js
@@ -2,16 +2,18 @@ import mongoose from "mongoose";
 import { BaseAuth } from "../auth/baseAuth.model.js";
 
 const parentSchema = new mongoose.Schema(
-  {
-    skaters: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Skater",
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
+    {
+        skaters: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Skater",
+            },
+        ],
+        default: [],
+
+    },
+    {
+        timestamps: true,
+    }
 );
 export const Parent = BaseAuth.discriminator("Parent", parentSchema);

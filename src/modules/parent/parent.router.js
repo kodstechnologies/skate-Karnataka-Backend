2ca18809bcfa22af86ab24c28bf95d5b8f1e29ba
sangram <balsangram1@gmail.com>
2026-05-28 10:16:35 +0530

    updated

--- src/modules/parent/parent.router.js
+++ src/modules/parent/parent.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { upload } from "../../middleware/multer.middleware.js";
+import { uploadAny } from "../../middleware/multer.middleware.js";
 import { validate } from "../../middleware/validate.multiple.js";
 import { authenticate } from "../../middleware/auth.middleware.js";
 import { afterLoginParentForm, displayAllParent, displayParentFullDetails } from "./parent.controller.js";
@@ -11,10 +11,7 @@ router.get("/v1/full-details/:id", authenticate(["Admin", "State"]), displayPare
 
 router.post(
   "/v1/after-login-parent-form/:id",
-  upload.fields([
-    { name: "img", maxCount: 1 },
-    { name: "document", maxCount: 1 }
-  ]),
+  uploadAny,
 //   validate(afterLoginParentFormValidation),
   afterLoginParentForm
 );

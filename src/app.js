615dbb7eb9fb6f7b72ea50400fcc38f315c295a5
sangram <balsangram1@gmail.com>
2026-07-06 15:19:02 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ app.use(cors({
 }));
 
 app.get('/health', (req, res) => {
-    res.send("Welcome tto KRSA backend .0!333 Server is healthy.00");
+    res.send("Welcome tto KRSA backend .0!333 Server is healthy.00 00vi hello");
 })
 
 app.use("/auth", authRouter);
